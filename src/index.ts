9608bbb07b49d45c014d0dbbec331a8651c1c7b2
Rasmus Eskola <fruitiex@gmail.com>
2019-01-25 20:58:01 +0200

    format with prettier, use "as any" syntax

--- src/index.ts
+++ src/index.ts
@@ -1,15 +1,15 @@
 import * as ex from 'excalibur';
 
 const game = new ex.Engine({
-    width: 800,
-    height: 600,
+  width: 800,
+  height: 600
 });
-const rectangle = new ex.Actor(150, game.drawHeight - 40, 200, 20)
+const rectangle = new ex.Actor(150, game.drawHeight - 40, 200, 20);
 rectangle.color = ex.Color.Yellow;
 rectangle.collisionType = ex.CollisionType.Fixed;
 game.add(rectangle);
 game.start();
 
-game.input.pointers.primary.on('move', (evt) => {
-    rectangle.pos.x = (<any>evt).worldPos.x
+game.input.pointers.primary.on('move', evt => {
+  rectangle.pos.x = (evt as any).worldPos.x;
 });
