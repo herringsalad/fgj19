0def9a2454e7c18110b51d86a4517c23226f0e72
Jaakko Hannikainen <jgke@jgke.fi>
2019-01-25 21:54:55 +0200

    Add grid

--- src/index.ts
+++ src/index.ts
@@ -1,20 +1,20 @@
 import * as ex from 'excalibur';
+import {Color, Vector} from 'excalibur';
 import {Mold} from "./mold";
-import {Vector} from "excalibur";
 
-const game = new ex.Engine({
-  width: 800,
-  height: 600
-});
-const rectangle = new ex.Actor(150, game.drawHeight - 40, 200, 20);
-rectangle.color = ex.Color.Yellow;
-rectangle.collisionType = ex.CollisionType.Fixed;
-game.add(rectangle);
+const width = 800;
+const height = 600;
 
-game.input.pointers.primary.on('move', evt => {
-  rectangle.pos.x = (evt as any).worldPos.x;
-});
+const game = new ex.Engine({width, height});
 
+for (let i = 0; i < Math.max(width/64, height/64); i++) {
+    const col = new ex.Actor(i * 64, 0,
+        1, height*2, Color.White);
+    game.add(col);
+    const row = new ex.Actor(0, i * 64,
+        width*2, 1, Color.White);
+    game.add(row);
+}
 
 game.add(new Mold(new Vector(500, 500)));
 game.add(new Mold(new Vector(400, 600)));
