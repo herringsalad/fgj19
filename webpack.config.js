7007341e077f12866368c783e6f378217b167564
Jaakko Hannikainen <jgke@jgke.fi>
2019-01-25 16:21:38 +0200

    Project base

---
+++ webpack.config.js
@@ -0,0 +1,42 @@
+'use strict';
+
+const webpack = require('webpack');
+const path = require('path');
+
+module.exports = {
+
+    mode: 'development',
+
+    entry: './src/index.ts',
+
+    output: {
+        path: path.resolve(__dirname, 'build'),
+        publicPath: '/build/',
+        filename: 'project.bundle.js'
+    },
+
+    module: {
+        rules: [
+            {
+                test: [ /\.vert$/, /\.frag$/ ],
+                use: 'raw-loader'
+            },
+            {
+                test: /\.tsx?$/,
+                use: 'ts-loader',
+                exclude: /node_modules/
+            }
+        ]
+    },
+
+    resolve: {
+        extensions: [ '.tsx', '.ts', '.js' ]
+    },
+
+    plugins: [
+        new webpack.DefinePlugin({
+            'CANVAS_RENDERER': JSON.stringify(true),
+            'WEBGL_RENDERER': JSON.stringify(true)
+        })
+    ]
+};
