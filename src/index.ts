e2fcab91cd23c298469dcd28b9e872cfd314484c
Rasmus Eskola <fruitiex@gmail.com>
2019-01-25 22:09:31 +0200

    Load randomized cheese tilemap

--- src/index.ts
+++ src/index.ts
@@ -1,22 +1,50 @@
 import * as ex from 'excalibur';
-import {Color, Vector} from 'excalibur';
-import {Mold} from "./mold";
+import { Color, Vector } from 'excalibur';
+import { Mold } from './mold';
 
 const width = 800;
 const height = 600;
 
-const game = new ex.Engine({width, height});
+const game = new ex.Engine({ width, height });
 
-for (let i = 0; i < Math.max(width/64, height/64); i++) {
-    const col = new ex.Actor(i * 64, 0,
-        1, height*2, Color.White);
-    game.add(col);
-    const row = new ex.Actor(0, i * 64,
-        width*2, 1, Color.White);
-    game.add(row);
+for (let i = 0; i < Math.max(width / 64, height / 64); i++) {
+  const col = new ex.Actor(i * 64, 0, 1, height * 2, Color.White);
+  game.add(col);
+  const row = new ex.Actor(0, i * 64, width * 2, 1, Color.White);
+  game.add(row);
 }
 
 game.add(new Mold(new Vector(500, 500)));
 game.add(new Mold(new Vector(400, 600)));
 
-game.start();
+const tileSheet = new ex.Texture('/assets/cheese.png');
+const loader = new ex.Loader([tileSheet]);
+
+const tm = new ex.TileMap({
+  x: 0,
+  y: 0,
+  cellWidth: 64,
+  cellHeight: 64,
+  rows: 12,
+  cols: 12
+});
+let spriteTiles = new ex.SpriteSheet(tileSheet, 1, 1, 64, 64);
+tm.registerSpriteSheet('default', spriteTiles);
+tm.data.forEach((cell: ex.Cell) => {
+  if (Math.random() > 0.5) {
+    cell.solid = true;
+    cell.pushSprite(new ex.TileSprite('default', 0));
+  }
+});
+
+const rectangle = new ex.Actor(150, game.drawHeight - 40, 200, 20);
+rectangle.color = ex.Color.Yellow;
+rectangle.collisionType = ex.CollisionType.Fixed;
+
+game.input.pointers.primary.on('move', evt => {
+  rectangle.pos.x = (evt as any).worldPos.x;
+});
+
+game.start(loader).then(() => {
+  game.add(tm);
+});
