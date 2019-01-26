f2178a1ce354b3d23dce56d2bed88b74de976201
Jaakko Hannikainen <jgke@jgke.fi>
2019-01-27 00:58:55 +0200

    Better game over

--- src/endscene.ts
+++ src/endscene.ts
@@ -1,4 +1,4 @@
-import {Actor, Color, Engine, EventTypes, Label, Scene, Vector} from "excalibur";
+import {Actor, Color, Engine, EventTypes, Label, Scene, TextAlign, Vector} from "excalibur";
 import {Game} from "./index";
 
 export class EndScene extends Scene {
@@ -15,17 +15,21 @@ export class EndScene extends Scene {
     const screen = new Actor(0, 0, engine.canvas.width, engine.canvas.height);
     screen.color = Color.fromHex("#FFD132");
 
-    const lostLabel = new Label(`You lost the game :(`, -450, -50, '');
-    const scoreLabel = new Label(`Your score: ${this.game.score}`, -450, 0, 'monospace');
+    const lostLabel = new Label(`You lost the game :(`, 0, -200, '');
+    const scoreLabel = new Label(`Your score: ${this.game.score}`, 0, 200, 'monospace');
 
-    const deadrat = new Actor( 300, 100, 204, 180);
-    deadrat.addDrawing(this.game.assets.deadMouse.asSprite());
-
-    const homeover = new Actor( 300, -100, 210, 108);
+    const homeover = new Actor( 0, -150, 210, 108);
     homeover.addDrawing(this.game.assets.homeover.asSprite());
+    homeover.body.pos.x = homeover.getCenter().x;
 
-    scoreLabel.scale = lostLabel.scale = new Vector(5, 5);
+    const deadrat = new Actor( 0, 50, 204, 180);
+    deadrat.addDrawing(this.game.assets.deadMouse.asSprite());
+    deadrat.body.pos.x = deadrat.getCenter().x;
+
+    scoreLabel.scale = lostLabel.scale = new Vector(3, 3);
     scoreLabel.color = lostLabel.color = Color.Black;
+    scoreLabel.body.pos.x = scoreLabel.getCenter().x;
+    scoreLabel.textAlign = TextAlign.Center;
 
     screen.add(scoreLabel);
     //screen.add(lostLabel);
