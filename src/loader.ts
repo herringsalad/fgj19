c8ebe671acf9c8ac6f7c4ede66778a8ee5913b55
Rasmus Eskola <fruitiex@gmail.com>
2019-01-27 12:19:30 +0200

    Fix splash screen background color

--- src/loader.ts
+++ src/loader.ts
@@ -1,6 +1,6 @@
-import {Color, ILoadable, Loader} from "excalibur";
-import {logo} from "./logo";
-import {DrawUtil} from "excalibur/dist/Util/Index";
+import { Color, ILoadable, Loader } from 'excalibur';
+import { logo } from './logo';
+import { DrawUtil } from 'excalibur/dist/Util/Index';
 
 export class Splash extends Loader {
   constructor(loadables: ILoadable[]) {
@@ -8,34 +8,38 @@ export class Splash extends Loader {
     this.logo = logo;
     this.logoWidth = 600;
     this.logoHeight = 600;
-    this.backgroundColor = '#FFD132';
+    this.backgroundColor = 'rgb(245, 209, 1)';
   }
-  
+
   startButtonFactory = () => {
     const buttonElement = document.createElement('button');
-    buttonElement.textContent = "";
+    buttonElement.textContent = '';
     buttonElement.id = 'start-button';
     buttonElement.style.display = 'none';
     buttonElement.style.alignContent = 'none';
     return buttonElement;
-  }
-  
+  };
+
   public draw(ctx: CanvasRenderingContext2D) {
     // @ts-ignore
     const engine = this._engine;
     let canvasHeight = engine.canvasHeight / window.devicePixelRatio;
     let canvasWidth = engine.canvasWidth / window.devicePixelRatio;
     let windowAspectRatio = window.innerWidth / window.innerHeight;
-    
+
     if (this._playButtonRootElement) {
-      let left = ctx.canvas.offsetLeft ;
+      let left = ctx.canvas.offsetLeft;
       let top = ctx.canvas.offsetTop;
       let buttonWidth = this._playButton.clientWidth;
       let buttonHeight = this._playButton.clientHeight;
-      this._playButtonRootElement.style.left = `${left + canvasWidth / 2 - buttonWidth / 2}px`;
-      this._playButtonRootElement.style.top = `${top + canvasHeight / 2 - buttonHeight / 2 + 100}px`;
+      this._playButtonRootElement.style.left = `${left +
+        canvasWidth / 2 -
+        buttonWidth / 2}px`;
+      this._playButtonRootElement.style.top = `${top +
+        canvasHeight / 2 -
+        buttonHeight / 2 +
+        100}px`;
     }
-    
 
     ctx.fillStyle = this.backgroundColor;
     ctx.fillRect(0, 0, canvasWidth, canvasHeight);
@@ -45,24 +49,30 @@ export class Splash extends Loader {
     var y = canvasHeight / 4;
     let mouseAspectRatio = 180 / 126;
     // var imageHeight = Math.floor(width * (this.logoHeight / this.logoWidth)); // OG height/width factor
-    
+
     var oldAntialias = engine.getAntialiasing();
     // engine.setAntialiasing(true);
     // console.log(window.innerWidth);
-    
-    ctx.drawImage(this._image, x, y, y * mouseAspectRatio * 1.5, y * windowAspectRatio);
-    
+
+    ctx.drawImage(
+      this._image,
+      x,
+      y,
+      y * mouseAspectRatio * 1.5,
+      y * windowAspectRatio
+    );
+
     // loading box
     // @ts-ignore
     if (!this.suppressPlayButton && this._playButtonShown) {
       engine.setAntialiasing(oldAntialias);
       return;
     }
-    
+
     var progBarWidth = window.innerWidth * 0.75;
     var progBarPosX = window.innerWidth / 2 - progBarWidth / 2;
-    var progBarPosY = window.innerHeight * .7;
-    
+    var progBarPosY = window.innerHeight * 0.7;
+
     ctx.lineWidth = 2;
     DrawUtil.roundRect(ctx, progBarPosX, progBarPosY, progBarWidth, 20, 10);
     // @ts-ignore
@@ -71,7 +81,16 @@ export class Splash extends Loader {
     var progressWidth = progress - margin * 2;
     var height = 20 - margin * 2;
     // @ts-ignore
-    DrawUtil.roundRect(ctx, progBarPosX + margin, progBarPosY + margin, progressWidth > 0 ? progressWidth : 0, height, 5, null, Color.White);
+    DrawUtil.roundRect(
+      ctx,
+      progBarPosX + margin,
+      progBarPosY + margin,
+      progressWidth > 0 ? progressWidth : 0,
+      height,
+      5,
+      null,
+      Color.White
+    );
     // engine.setAntialiasing(oldAntialias);
   }
 }
